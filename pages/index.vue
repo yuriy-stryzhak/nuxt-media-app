50a78fd93ec140006fc69c95a9625f1010808b76
YS <yuriy.stryzhak@gmail.com>
2024-10-21 15:56:25 +0300

    Connect and config Vuetify

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-    <h1>Main</h1>
+    <v-container>
+        <h1 class="text-center text-h3">Home</h1>
+        <v-btn color="primary">Hello Vuetify</v-btn>
+    </v-container>
 </template>
 
 <script setup lang="ts">
---
+++ plugins/vuetify.ts
@@ -0,0 +1,31 @@
+// plugins/vuetify.ts
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import '@mdi/font/css/materialdesignicons.css';
+import 'vuetify/styles';
+
+export default defineNuxtPlugin((nuxtApp) => {
+  const vuetify = createVuetify({
+    components,
+    directives,
+    theme: {
+      defaultTheme: 'light',
+      themes: {
+        light: {
+          colors: {
+            primary: '#1976D2',
+            secondary: '#424242',
+            accent: '#82B1FF',
+            error: '#FF5252',
+            info: '#2196F3',
+            success: '#4CAF50',
+            warning: '#FFC107'
+          }
+        }
+      }
+    }
+  });
+
+  nuxtApp.vueApp.use(vuetify);
+});
