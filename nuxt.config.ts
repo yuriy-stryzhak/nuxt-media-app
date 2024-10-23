8c87ede486c2dc3e51eca8a3b1c73fa85a3bf649
YS <yuriy.stryzhak@gmail.com>
2024-10-23 19:02:07 +0300

    Added config for deploy to github page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,17 @@ import vuetify from 'vite-plugin-vuetify';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/nuxt-media-app_preview/',
+  },
+  generate: {
+    routes: ['/']
+  },
+  router: {
+    options: {
+      hashMode: true,
+    },
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['@/assets/styles/main.scss'],
